a849bf275d1036d0d163e0c03a14e14707e1e9d6
Timothy  Gee <thetimgee@Timothys-MBP.lan>
2023-11-27 23:01:38 -0800

    finished navbar, as well as hamburger menu on mobile

--- portfolio/src/app/components/HeaderSection.jsx
+++ portfolio/src/app/components/HeaderSection.jsx
@@ -26,11 +26,11 @@ const HeaderSection = () => {
 
           <div>
             <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple500 to-pink-500 hover:bg-slate-200 text-white">
-              Hire Me
+              Contact Me
             </button>
             <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border mt-3">
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                View CV
               </span>
             </button>
           </div>
